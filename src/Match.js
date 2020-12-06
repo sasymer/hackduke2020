bae143937bbfe4ad148d50cad5ed5348c2a10ce1
sas182 <sas182@duke.edu>
2020-12-06 00:43:33 -0600

    maatch

--- src/Match.js
+++ src/Match.js
@@ -5,9 +5,39 @@ import Nav from './Nav';
 import { Link } from 'react-router-dom';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import MultiSelect from 'react-multi-select-component';
+import Slider from 'react-input-slider';
 
 function Match() {
   const username = localStorage.getItem('username');
+  let myFormat = 'Teletherapy';
+  let myGender = '';
+  const [myEths, setEths] = useState([]);
+  const [formatI, setFormatI] = useState({x: 0});
+
+  const format = [
+    'In-person', 'Teletherapy', 'Group Therapy'
+  ];
+
+  const gender = [
+    'Male', 'Female', 'Non-binary'
+  ];
+
+  const age = [
+    '0-6', '6-10', '11-13', '14-19', 'Adult', '65+'
+  ];
+
+  const ethnicity = [
+    'African American', 'Hispanic and Latino', 'Asian', 'Native American', 'Pacific Islander', 'Other'
+  ];
+
+  const eth = [
+    {label: "African American", value: "African American"},
+    {label: "Hispanic/Latino", value: "Hispanic/Latino"},
+    {label: "Asian", value: "Asian"},
+    {label: "Native American", value: "Native American"},
+    {label: "Pacific Islander", value: "Pacific Islander"}
+  ];
 
   return (
     <div>
@@ -19,19 +49,46 @@ function Match() {
         </div>
       </div>
 
-      <div className = "float">
-        <p> What therapy format are you looking for? </p>
-        
-      </div>
+      <div>
+        <div className = "float">
+            <p> What therapy format are you looking for? </p>
+            <Dropdown 
+              options={format} 
+              onChange={(e) => {
+                myFormat = e.value;
+              }}
+            />
+            <br/>
+            <p> How important is this to you? {formatI.x} </p>
+            <Slider 
+                axis = "x"
+                xstep={1}
+                xmin={0}
+                xmax={10}
+                x={formatI.x}
+                onChange={({ x }) => setFormatI({ x: x})}
+            />
+            
+        </div>
 
-      <div className = "float">
-        <p> Any preference for the therapist's gender? </p>
-        
-      </div>
+        <div className = "float">
+            <p> Any preference for the therapist's gender? </p>
+            <Dropdown 
+              options={gender}
+              onChange={(e) => {
+                myGender = e.value;
+              }}
+            />
+        </div>
 
-      <div className = "float">
-        <p> Any preference for the therapist's ethnicity? </p>
-        
+        <div className = "float">
+            <p> Any preference for the therapist's ethnicity? </p>
+            <MultiSelect 
+              options={eth}
+              value = {myEths}
+              onChange = {setEths}
+            />
+        </div>
       </div>
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -6,32 +6,27 @@ function Nav() {
     const type = localStorage.getItem('type');
     const navStyle = {
         color: 'black',
-        fontWeight: 'bold',
-        textDecoration: 'none',
-        background: 'whitesmoke',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-        padding: '10px',
-        borderRadius: '5px'
+        textDecoration: 'none'
     };
 
     return (
         <nav>
             <u1 className="navlinks">
-            <Link style={navStyle} to='/'>
-                <li>Home</li>
-            </Link>
-            <Link style={navStyle} to='/all'>
-                <li>All Therapists</li>
-            </Link>
-            <Link style={navStyle} to='/match'>
-                <li>Find my match</li>
-            </Link>
-            <Link style={navStyle} to='/resources'>
-                <li>Resources</li>
-            </Link>
-            <Link style ={navStyle} to = '/login'>
-                <li>Logout</li> 
-            </Link>
+                <Link style={navStyle} to='/'>
+                    <li>Home</li>
+                </Link>
+                <Link style={navStyle} to='/all'>
+                    <li>All Therapists</li>
+                </Link>
+                <Link style={navStyle} to='/match'>
+                    <li>Find my match</li>
+                </Link>
+                <Link style={navStyle} to='/resources'>
+                    <li>Resources</li>
+                </Link>
+                <Link style ={navStyle} to = '/login'>
+                    <li>Logout</li> 
+                </Link>
             </u1>
         </nav>   
     );
