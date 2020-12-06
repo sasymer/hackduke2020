7993fad9b8d3da5da101221bdcfa4e9b4f2ac79b
sas182 <sas182@duke.edu>
2020-12-05 23:08:19 -0600

    exact

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Newuser from './Newuser';
 import Match from './Match';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './Login';
+import Resources from '/.Resources.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -16,11 +17,11 @@ function App() {
       <div className="App">
           <Switch>
             <Route path="/login" exact component={Login}/>
-            <Route path="/newuser" component={Newuser}/>
-            <Route path ="/" component ={Home}/>
+            <Route path="/newuser"  component={Newuser}/>
+            <Route path ="/" exact component ={Home}/>
             <Route path ="/all" component ={All}/>
             <Route path ="/match" component ={Match}/>
-            <Route path ="/resources" component ={Match}/>
+            <Route path ="/resources" component ={Resources}/>
           </Switch>
       </div>
     </Router>
